1a91257bffe330730e8df015cb2f30a7c19b4d0f
Jxdn <hashertechnical15@gmail.com>
2026-01-07 18:24:49 -0500

    Client | Initialized Frontend React TS

--- src/background/Router.js
+++ src/background/Router.js
@@ -1,15 +1,11 @@
-const { json } = require("express");
+const express = require("express");
 const calc = require("./Calc");
 const error = require("./ErrorHandler");
 
 class Router {
     constructor(server){
         this.server = server;
-        server.use(json());
-
-        server.get("/", (req, res)=> {
-            res.status(200);
-        });
+        server.use(express.json());
 
         server.post("/api/calculate", (req, res)=> {
             try {
@@ -18,8 +14,9 @@ class Router {
                 error({message: e, status: 400}, req, res);
             };
         });
-    };
 
+        server.use(express.static(`${process.cwd()}/src/client/dist`))
+    };
 
     initialize(port){
         console.log("Server Initializing...");
