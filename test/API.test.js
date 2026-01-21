7219d3275277645894cd575c8d0fefc9ba18f0af
Jxdn <hashertechnical15@gmail.com>
2026-01-21 18:09:04 -0500

    Infrastructure | Implementing testing via Jest

---
+++ test/API.test.js
@@ -0,0 +1,33 @@
+const { Server } = require("tls");
+const { testData, routes, expectedResult } = require("./Constants");
+const server = require("../src/controller/Server");
+
+test("api/calculate Route Test", async ()=> {
+    let routeAddr;
+    let agent = server.listen(0);
+    let { address, port } = agent.address();
+    address = (address===("::"||"0.0.0.0") ? "localhost" : address);
+    let addr = (agent instanceof Server ? "https" : "http") + "://" + address + ":" + port;
+    routeAddr = addr + routes["calc"];
+    console.log(routeAddr)
+    let options = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(testData)
+    };
+    let res = await fetch(routeAddr, options);
+    let status = res.status;
+    let body = await res.json();
+    expect(status).toBe(200);
+    expectedResult.forEach((e)=> {
+        expect(body).toHaveProperty(e)
+    });
+    new Promise((resolve) => {
+        agent.closeAllConnections();
+        agent.close(()=> {
+            resolve();
+        });
+    });
+});
---
+++ test/Constants.js
@@ -0,0 +1,14 @@
+module.exports = {
+    testData: {
+        data: {
+            h: 305,
+            t: 275,
+            w: 3900,
+            d: "AWD"
+        }
+    },
+    routes: {
+        calc: "/api/calculate"
+    },
+    expectedResult: ["zeroToSixty", "quarterMile", "trapSpeed"]
+};
