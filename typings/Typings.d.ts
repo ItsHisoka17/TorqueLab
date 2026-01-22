f33b0e94e1f775c5b9c8f958e3fa981cbf5e2c2d
Jxdn <hashertechnical15@gmail.com>
2026-01-22 02:01:58 -0500

    Typings | Added Express and ErrorHandler typings

--- typings/Typings.d.ts
+++ typings/Typings.d.ts
@@ -10,3 +10,12 @@ export interface CalcResult {
     quarterMile: number,
     trapSpeed: number
 }
+
+export interface ErrorParams {
+    message: string,
+    status: "400" | "200" | "401" | "404" | "500"
+}
+
+export type App = import ("express").Application
+export type Request = import ("express").Request
+export type Response = import ("express").Response
